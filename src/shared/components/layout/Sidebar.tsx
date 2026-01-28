4f1c35456548784d6b07bf98954d3c903629ea1b
Abdulah Sarajlic <abdulah.sarajlic@artcogroup.ba>
2026-01-28 15:32:31 +0100

    feat(comp): ARTCOCRM-228 resolved conflicts

--- src/shared/components/layout/Sidebar.tsx
+++ src/shared/components/layout/Sidebar.tsx
@@ -52,6 +52,8 @@ const navigation = [
     href: PAGE_ROUTES.TESTING.LIST,
     icon: 'tasks' as const,
     roles: [UserRole.ENG_LEAD, UserRole.ADMIN],
+  },
+  {
     name: 'Notes',
     href: PAGE_ROUTES.NOTES.ROOT,
     icon: 'notes' as const,
