99cce63655caa4cec6d27ff4c2da37c0a4e19f4a
Abdulah <abdulah.sarajlic@artcogroup.ba>
2026-02-02 14:05:43 +0100

    feat(badge): ARTCOCRM-211 conflicts resolved

--- src/shared/components/ui/badge.tsx
+++ src/shared/components/ui/badge.tsx
@@ -1,10 +1,24 @@
+import * as React from 'react';
 import { type ReactNode } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
+import { cn } from '@/lib/utils';
+
 const badgeVariants = cva(
-  'inline-flex items-center gap-1.5 px-2.5 py-1 text-xs font-medium whitespace-nowrap',
+  'inline-flex items-center gap-1.5 px-2.5 py-1 text-xs',
   {
     variants: {
+      variant: {
+        default:
+          'rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80',
+        secondary:
+          'font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2rounded-md border border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        destructive:
+          'rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+        outline:
+          'rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-foreground',
+        basic: 'font-medium whitespace-nowrap',
+      },
       size: {
         sm: 'px-2 py-0.5 text-[11px]',
         md: 'px-2.5 py-1 text-xs',
@@ -12,25 +26,36 @@ const badgeVariants = cva(
       },
     },
     defaultVariants: {
+      variant: 'basic',
       size: 'md',
     },
   }
 );
 
-export interface BadgeProps extends VariantProps<typeof badgeVariants> {
+export interface BadgeProps
+  extends
+    React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {
   children?: ReactNode;
   icon?: ReactNode;
   className?: string;
 }
 
-export const Badge = ({ size, children, icon, className }: BadgeProps) => {
+export const Badge = ({
+  size,
+  children,
+  icon,
+  className,
+  variant,
+  ...props
+}: BadgeProps) => {
   return (
-    <span className={badgeVariants({ size, className })}>
+    <div className={cn(badgeVariants({ variant }), className, size)} {...props}>
       {icon && (
         <span className="inline-flex shrink-0 items-center">{icon}</span>
       )}
       {children}
-    </span>
+    </div>
   );
 };
 
